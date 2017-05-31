46913692597f6eb518294706c91415b0cd5bd6ea
artmario <artmario@gmail.com>
2017-05-31 19:45:25 -0400

    teste

--- primeiroArray/primeiroArray.java
+++ primeiroArray/primeiroArray.java
@@ -5,7 +5,7 @@ public class PrimeiroArray {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+        System.out.println("ola mundo");
     }
     
 }

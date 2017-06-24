38b080c4975b6a29ee3f51784c23a57f8218a8f8
artmario <artmario@gmail.com>
2017-06-24 14:16:32 -0400

    inicio exercicio banco

---
+++ banco/conta.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author artmario
+ */
+public class conta
+{
+    private String num;
+    private float saldo;
+
+    public conta(String num, float saldo)
+    {
+        this.num = num;
+        this.saldo = saldo;
+    }
+    
+    
+}
